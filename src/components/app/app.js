e759f4f1cbcf522362568cbae67682390f3a8d94
Ruslan Robotko <robotko.ruslan@gmail.com>
2020-12-27 16:12:05 +0200

    finish project

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -32,7 +32,7 @@ export default class App extends Component {
   };
 
   render() {
-    const char = this.state.showRandomChar ? <RandomChar  /> : null;
+    const char = this.state.showRandomChar ? <RandomChar interval={15000} /> : null;
     if (this.state.error) {
       return <ErrorMessage />;
     }
--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,55 +1,44 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./itemList.css";
 import Spinner from "../spinner/spinner";
-import PropTypes from "prop-types";
 
-export default class ItemList extends Component {
-  state = {
-    itemList: null,
-  };
 
-  static defaultProps = {
-    onItemSelected: () => {},
-  };
-  
-  static propTypes = {
-    onItemSelected: PropTypes.func,
-    getData: PropTypes.arrayOf(PropTypes.objstatic  
 
-  componentDidMount() {
-    const { getData } = this.props;
+function ItemList ({getData, onItemSelected, renderItem}){
+ const [itemList, updateList] = useState([])
 
-    getData().then((itemList) => {
-      this.setState({
-        itemList,
-      });
-    });
-  }
+useEffect(() =>{
+  getData()
+  .then((data) => {
+    updateList(data)
+  })
+}, [])
 
-  renderItems(arr) {
+
+  function renderItems(arr) {
     return arr.map((item) => {
-      const { id } = item;
-      const label = this.props.renderItem(item);
+      const {id} = item;
+      const label = renderItem(item)
       return (
         <li
           key={id}
           className="list-group-item"
-          onClick={() => this.props.onItemSelected(id)}
-        >
+          onClick={ () => onItemSelected(id)}>
           {label}
         </li>
-      );
-    });
+      )
+    })
   }
 
-  render() {
-    const { itemList } = this.state;
+  
+    
 
     if (!itemList) {
       return <Spinner />;
     }
-    const items = this.renderItems(itemList);
-    return <ul className="item-list list-group">{items}</ul>;
-  }
+    const items = renderItems(itemList);
+    return (<ul className="item-list list-group">{items}</ul>)
+
 }
 
+export default ItemList
